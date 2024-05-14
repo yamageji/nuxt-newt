01b38f7c49f35c864ebba7b711098fa5c631e37e
yamageji <koyama.shigehito.chabatake102@gmail.com>
2024-05-14 22:58:16 +0900

    feat: add modules.ts

--- src/module.ts
+++ src/module.ts
@@ -1,19 +1,53 @@
-import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit';
+import { defineNuxtModule, addImportsDir, createResolver } from '@nuxt/kit';
+import { defu } from 'defu';
 
 // Module options TypeScript interface definition
-export interface ModuleOptions {}
+export interface ModuleOptions {
+  spaceUid: string;
+  cdnApiToken: string;
+  apiType?: 'cdn' | 'api';
+  target?: 'all' | 'server';
+}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: 'my-module',
-    configKey: 'myModule',
+    name: 'nuxt-newt',
+    configKey: 'newt',
+    compatibility: {
+      nuxt: '^3.0.0',
+    },
   },
-  // Default configuration options of the Nuxt module
-  defaults: {},
-  setup(_options, _nuxt) {
-    const resolver = createResolver(import.meta.url);
+  setup(options, nuxt) {
+    if (!options.spaceUid) {
+      throw new Error('spaceUid is required');
+    }
+    if (!options.cdnApiToken) {
+      throw new Error('cdnApiToken is required');
+    }
+    if (!options.apiType) {
+      options.apiType = 'cdn';
+    }
+    if (!options.target) {
+      options.target = 'server';
+    }
 
-    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolver.resolve('./runtime/plugin'));
+    nuxt.options.runtimeConfig.public.newt = defu(
+      nuxt.options.runtimeConfig.public.newt, {
+        spaceUid: options.spaceUid,
+        cdnApiToken: options.cdnApiToken,
+        apiType: options.apiType,
+      });
+
+    nuxt.options.runtimeConfig.newt = defu(
+      nuxt.options.runtimeConfig.newt, {
+        cdnApiToken: options.cdnApiToken,
+      });
+
+    if (!nuxt.options.dev && options.target !== 'all') {
+      nuxt.options.runtimeConfig.public.newt.cdnApiToken = '';
+    }
+
+    const { resolve } = createResolver(import.meta.url);
+    addImportsDir(resolve('./runtime/composables'));
   },
 });
