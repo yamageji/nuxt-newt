d7f5446c16da7b7ff747742514d39247b9fe6082
yamageji <koyama.shigehito.chabatake102@gmail.com>
2024-05-14 22:55:47 +0900

    fix: eslint stylistic settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
+import { createConfigForNuxt } from '@nuxt/eslint-config/flat';
 
 // Run `npx @eslint/config-inspector` to inspect the resolved config interactively
 export default createConfigForNuxt({
@@ -7,14 +7,16 @@ export default createConfigForNuxt({
     // Rules for module authors
     tooling: true,
     // Rules for formatting
-    stylistic: true,
+    stylistic: {
+      indent: 2,
+      quotes: 'single',
+      semi: true,
+    },
   },
   dirs: {
-    src: [
-      './playground',
-    ],
+    src: ['./playground'],
   },
 })
-  .append(
-    // your custom flat config here...
-  )
+  .append
+  // your custom flat config here...
+  ();
