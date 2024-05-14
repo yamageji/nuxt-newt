57d4323755b60098ecf42e9cf832e7965eed338c
yamageji <koyama.shigehito.chabatake102@gmail.com>
2024-05-14 22:59:16 +0900

    feat: add composables

---
+++ src/runtime/composables/useNewtClient.ts
@@ -0,0 +1,59 @@
+import { createClient } from 'newt-client-js';
+import type {
+  GetContentsParams,
+  GetContentParams,
+  GetFirstContentParams,
+} from 'newt-client-js/dist/types';
+
+import { useRuntimeConfig, useAsyncData } from 'nuxt/app';
+
+const newtClient = () => {
+  const config = useRuntimeConfig();
+  return createClient({
+    spaceUid: config.public.newt.spaceUid,
+    token: config.public.newt.cdnApiToken
+      ? config.public.newt.cdnApiToken
+      : config.newt.cdnApiToken,
+    apiType: config.public.newt.apiType,
+  });
+};
+
+export const useNewtGetContents = <T>(
+  key: string,
+  { appUid, modelUid, query }: GetContentsParams,
+) => {
+  return useAsyncData(key, async () => {
+    return await newtClient().getContents<T>({
+      appUid: appUid,
+      modelUid: modelUid,
+      query: query,
+    });
+  });
+};
+
+export const useNewtGetContent = <T>(
+  key: string,
+  { appUid, modelUid, contentId, query }: GetContentParams,
+) => {
+  return useAsyncData(key, async () => {
+    return await newtClient().getContent<T>({
+      appUid: appUid,
+      modelUid: modelUid,
+      contentId: contentId,
+      query: query,
+    });
+  });
+};
+
+export const useNewtGetFirstContent = <T>(
+  key: string,
+  { appUid, modelUid, query }: GetFirstContentParams,
+) => {
+  return useAsyncData(key, async () => {
+    return await newtClient().getFirstContent<T>({
+      appUid: appUid,
+      modelUid: modelUid,
+      query: query,
+    });
+  });
+};
